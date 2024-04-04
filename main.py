dda2e02a8c2aa271b744af6902c68473898fa171
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-04-04 09:44:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,24 +26,27 @@ def roll():
 
 
 root = ctk.CTk()
-root.geometry("400x500")
+root.geometry("325x425")
 root.title("Roll")
 root.resizable(False, False)
 
-titolo = ctk.CTkLabel(master=root, text="Tira dadi", font=("Calibri", 40))
-titolo.pack(pady=5)
+frameDanni = ctk.CTkFrame(master=root)
+frameDanni.pack(pady=20)
 
-textbox = ctk.CTkTextbox(master=root, width=150, height=10, fg_color="#d2d2d2", text_color="#333333")
+titolo = ctk.CTkLabel(master=frameDanni, text="Tira dadi danni", font=("Calibri", 40))
+titolo.pack(pady=5, padx=20)
+
+textbox = ctk.CTkTextbox(master=frameDanni, width=150, height=10, fg_color="#d2d2d2", text_color="#333333")
 textbox.pack(pady=15)
 
-tira = ctk.CTkButton(master=root, text="Tira", command=roll)
+tira = ctk.CTkButton(master=frameDanni, text="Tira", command=roll)
 tira.pack(pady=5)
 
 img = ctk.CTkImage(Image.open("d20.png"), size=(150, 150))
-imgL = ctk.CTkLabel(master=root, image=img, text="")
+imgL = ctk.CTkLabel(master=frameDanni, image=img, text="")
 imgL.pack(pady=10)
 
-totale = ctk.CTkLabel(master=root, text="Totale tiro:", font=("Calibri", 25))
-totale.pack(pady=5)
+totale = ctk.CTkLabel(master=frameDanni, text="Totale tiro:", font=("Calibri", 25))
+totale.pack(pady=10)
 
 root.mainloop()
